ab35bf69ebafce053d1e4e0be82c6cf98f61ec27
Gustavo Felix <gusfelix2015@gmail.com>
2021-02-17 18:53:06 -0300

    Configure query create, find and update

--- src/app/models/Member.js
+++ src/app/models/Member.js
@@ -19,8 +19,9 @@ module.exports = {
          birth,
          blood,
          weigth,
-         heigth
-        ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+         heigth,
+         instructor_id
+        ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
         RETURNING id
     `;
 
@@ -33,6 +34,7 @@ module.exports = {
       data.blood,
       data.weigth,
       data.heigth,
+      data.instructor,
     ];
 
     db.query(query, values, function (err, results) {
@@ -43,9 +45,10 @@ module.exports = {
 
   find(id, callback) {
     db.query(
-      `SELECT * 
+      `SELECT members.*, instructors.name AS instructor_name
     FROM members 
-    WHERE id = $1`,
+    LEFT JOIN instructors ON (members.instructor_id = instructors.id) 
+    WHERE members.id = $1`,
       [id],
       function (err, results) {
         if (err) throw `Database Error ${err}`;
@@ -64,8 +67,9 @@ module.exports = {
       email=($5),
       blood=($6),
       weigth=($7),
-      heigth=($8)
-    WHERE id = $9
+      heigth=($8),
+      instructor_id=($9)
+    WHERE id = $10
     `;
 
     const values = [
@@ -77,6 +81,7 @@ module.exports = {
       data.blood,
       data.weigth,
       data.heigth,
+      data.instructor,
       data.id,
     ];
     db.query(query, values, (err, results) => {
@@ -89,6 +94,13 @@ module.exports = {
     db.query(`DELETE FROM members WHERE id = $1`, [id], (err, results) => {
       if (err) throw `Database Error ${err}`;
       return callback();
+    });
+  },
+
+  instructorsSelectOption(callback) {
+    db.query(`SELECT name,id FROM instructors`, (err, results) => {
+      if (err) throw `Database Error ${err}`;
+      callback(results.rows);
     });
   },
 };
