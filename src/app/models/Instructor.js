51c5ebccbc41d8ccaca01823c11085d994fdc89b
Gustavo Felix <gusfelix2015@gmail.com>
2021-02-17 18:52:00 -0300

    Querry count members in instructors

--- src/app/models/Instructor.js
+++ src/app/models/Instructor.js
@@ -3,10 +3,17 @@ const db = require("../../config/db");
 
 module.exports = {
   all(callback) {
-    db.query(`SELECT * FROM instructors ORDER BY name ASC`, function (err, results) {
-      if (err) throw `Database Error ${err}`;
-      callback(results.rows);
-    });
+    db.query(
+      `SELECT instructors.*, count(members) AS total_students
+       FROM instructors
+       LEFT JOIN members ON (members.instructor_id = instructors.id)
+       GROUP BY instructors.id 
+       ORDER BY total_students DESC`,
+      (err, results) => {
+        if (err) throw `Database Error ${err}`;
+        callback(results.rows);
+      }
+    );
   },
 
   create(data, callback) {
