51fb17d5aec88894fb6bf59031997b9b1243e477
Gustavo Felix <gusfelix2015@gmail.com>
2021-03-02 21:19:58 -0300

    Introduction paginate in controllers

--- src/app/controlers/instructors.js
+++ src/app/controlers/instructors.js
@@ -15,21 +15,19 @@ module.exports = {
       limit,
       offset,
       callback(instructors) {
-        return res.render("instructors/index", { instructors, filter });
+        const pagination = {
+          total: Math.ceil(instructors[0].total / limit),
+          page,
+        };
+        return res.render("instructors/index", {
+          instructors,
+          pagination,
+          filter,
+        });
       },
     };
 
     Instructor.paginate(params);
-
-    // if (filter) {
-    //   Instructor.findby(filter, (instructors) => {
-    //     return res.render("instructors/index", { instructors, filter });
-    //   });
-    // } else {
-    //   Instructor.all(function (instructors) {
-    //     return res.render("instructors/index", { instructors });
-    //   });
-    // }
   },
 
   create(req, res) {
